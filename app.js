298f5e5836e0562eea0eba4823e7de9948d1c2b7
zhalice2011 <zhalice2012@gmail.com>
2020-03-12 23:13:33 +0800

    add hooks

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
-const path = require('path')
+const Koa = require('koa')
+const Router = require('koa-router')
+const favicon = require('koa-favicon');
+const app = new Koa()
+const router = new Router()
 
-const Koa = require('koa');
-const app = new Koa();
-const route = require('koa-route');
+const views = require('koa-views')
+const co = require('co')
+const convert = require('koa-convert')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+const debug = require('debug')('koa2:server')
+const path = require('path')
 const serve = require('koa-static');
+
+const config = require('./config')
+const routes = require('./routes')
+
+const port = process.env.PORT || config.port
+
+// error handler
+onerror(app)
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - $ms`)
+})
+
+// favicon
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+routes(router)
+app.on('error', function(err, ctx) {
+  console.log(err)
+  logger.error('server error', err, ctx)
+})
 
 const home   = serve(path.join(__dirname)+'/public/');
 
-const hello = ctx => {
-  ctx.response.body = 'Hello World';
-};
-
-// 3.分配路由
 app.use(home); 
-app.listen(3000);
+
+module.exports = app.listen(config.port, () => {
+  console.log(`Listening on http://localhost:${config.port}`)
+})
